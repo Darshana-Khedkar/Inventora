585321a06c39d9eeb2fa1237e87c3c00666d23a7
Darshana-Khedkar <khedkar440@gmail.com>
2025-08-10 13:23:05 +0530

    Updated seed.js

--- backend/seed.js
+++ backend/seed.js
@@ -2,8 +2,8 @@
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import dotenv from "dotenv";
-import User from "./models/userModel.js";
-import Product from "./models/productModel.js";
+import User from "./models/User.js";
+import Product from "./models/Product.js";
 
 dotenv.config();
 
